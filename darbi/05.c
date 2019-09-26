9788a530fc17fc65ff7730bbeb51d9a337e62933
user <rinis.pastars2000@gmail.com>
2019-09-26 15:50:47 +0300

    20190926_15_50

---
+++ darbi/05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+	printf ("ievadi simbolu :");
+	char abc;
+	scanf ("%c", &abc);
+
+	printf ("%c \n", abc );
+	printf ("%d \n", abc );
+	printf ("%x \n", abc );
+}
