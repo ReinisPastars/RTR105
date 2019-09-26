6d0ef7f2736564936515b175cd6597544e3a9ffc
user <rinis.pastars2000@gmail.com>
2019-09-26 15:19:23 +0300

    20190926_15_20

---
+++ darbi/03.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+ {
+//"Hello world!");
+//printf(Hello world!/n");
+printf("Hello world!\n");
+printf("I like trains\a\n");
+printf("Big\n \rOr\n \rSmall\n");
+printf("Like them all!!!\n");
+ }
+
---
+++ darbi/04.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float pi()
+{
+return 3.14;
+}
+int main()
+{
+int R=143 ;
+int L=2*pi()*R ;
+printf("Rinka ar radiusu %d garums ir %d m\n", R, L);
+return 0;
+}
+
