46b65fec5d917fd472c5b0131a70d4bd7953cfb8
user <rinis.pastars2000@gmail.com>
2019-10-17 15:55:24 +0300

    20191017_16_00

---
+++ darbi/user_function_no_arg_no_return.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void user_function_1()
+ {
+ printf("Cienījamais lietotāj, sveicu tevi no savas lietotāja funkcijas!\n");
+ }
+
+void user_function_2();
+ 
+void main()
+ {
+ user_function_1();
+ user_function_1();
+ user_function_2();
+ }
+
+void user_function_2()
+ {
+ user_function_1();
+ }
