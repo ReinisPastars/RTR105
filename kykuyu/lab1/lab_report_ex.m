c999dfb29fe8ab0b166c9733c8b730b8f0fdef1c
ReinisPastars <54942907+ReinisPastars@users.noreply.github.com>
2020-02-10 10:05:53 +0200

    Add files via upload

---
+++ kykuyu/lab1/lab_report_ex.m
@@ -0,0 +1,13 @@
+%% 1.laboratorijas darbs
+%% Mērījumu datu apstrāde
+%% Mērķi:
+% * Iemācīties apstrādāt mērījumu datus
+% * Iemācīties polyfit polyval funkcijas
+% * Iemācīties veidot matlab atskaites izmantojot "publish"
+%% Darba programma:
+t = 0:0.01:1;
+y = sin(2*pi*t);
+plot(t,y)
+%% Secinājumi
+% Secinājumos raksta par to ko esam iemācījušīes šajā darbā, vai arī
+% skatamies darba mērķi un rakstam secinājumus.
